91fef55fc19de7c163acf6239648086c1503205a
GG-Afterdeath <maicoljosue@outlook.com>
2023-09-06 11:07:58 -0500

    first commit

--- app/src/main/java/co/edu/sena/ar2687386michaelrico/MainActivity.kt
+++ app/src/main/java/co/edu/sena/ar2687386michaelrico/MainActivity.kt
@@ -22,7 +22,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("App Reader 2687386")
+                    Greeting("App - Reader - 2687386")
                 }
             }
         }
